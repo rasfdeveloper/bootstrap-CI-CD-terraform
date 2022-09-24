e9987b411f96b74f36082cdabbbe97f3bbd785a3
Renato Franco <renatofr95@gmail.com>
2022-09-24 16:14:22 -0300

    Change terraform version

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
-# Require TF version to be same as or greater than 0.12.13
+# Require TF version to be same as or greater than 1.1.5
 terraform {
-  required_version = ">=0.12.13"
+  required_version = ">=1.1.5"
   backend "s3" {
    bucket         = "state-tf-bucket"
    key            = "terraform.tfstate"
@@ -13,7 +13,7 @@ terraform {
 # Download any stable version in AWS provider of 2.36.0 or higher in 2.36 train
 provider "aws" {
   region  = "sa-east-1"
-  version = "~> 2.36.0"
+  version = "~> 4.32.0"
 }
 
 # Call the seed_module to build our ADO seed info
